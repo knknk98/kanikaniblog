7be41d33501de88fad9a05c15ce21f950e4eb529
knknk98 <kayooshino@gmail.com>
2021-06-05 21:00:01 +0900

    ブログリストを作成日の降順に

--- kanikaniblog/urls.py
+++ kanikaniblog/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
